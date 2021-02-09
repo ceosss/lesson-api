7100134ddf0ad5922e824deee3a47d2468a20096
ceosss <sswarajsamant@gmail.com>
2021-02-09 16:24:43 +0530

    UPDATE: Add Comments

--- router/router.go
+++ router/router.go
@@ -13,11 +13,15 @@ func NewRouter() *mux.Router {
 	router := mux.NewRouter()
 
 	// All the handlers
+
+	// Model Handlers
 	router.HandleFunc("/model", modelhandlers.AllModels).Methods("GET")
 	router.HandleFunc("/model", modelhandlers.CreateModel).Methods("POST")
 	router.HandleFunc("/model/{id}", modelhandlers.SingleModel).Methods("GET")
 	router.HandleFunc("/model/{id}", modelhandlers.UpdateModel).Methods("PUT")
 	router.HandleFunc("/model/{id}", modelhandlers.DeleteModel).Methods("DELETE")
+
+	// Lesson Handlers
 	router.HandleFunc("/lesson", lessonhandlers.AllLessons).Methods("GET")
 	router.HandleFunc("/lesson", lessonhandlers.CreateLesson).Methods("POST")
 	router.HandleFunc("/lesson/{id}", lessonhandlers.SingleLesson).Methods("GET")
